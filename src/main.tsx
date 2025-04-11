a537327c90ca3812dda1f5ba638a6d045ce9bf91
Tetjana Strutynska <tetjana00iris@gmail.com>
2025-04-11 00:23:21 -0300

    #chszHaH9 browser router

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,24 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import {RouterProvider} from "react-router-dom";
-import {routes} from "./router/routes.tsx";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import {UsersComponent} from "./components/users-component/UsersComponent.tsx";
+import {ProductsComponent} from "./components/products-component/ProductsComponent.tsx";
+import {PostsComponent} from "./components/posts-component/PostsComponent.tsx";
+import App from "./App.tsx";
+// import {routes} from "./router/routes.tsx";
+
+// createRoot(document.getElementById('root')!)
+//     .render(<RouterProvider router={routes}/>)
 
 createRoot(document.getElementById('root')!)
-    .render(<RouterProvider router={routes}/>)
+    .render(<BrowserRouter>
+        <Routes>
+            <Route path={'/'} element={<App/>}>
+                <Route path={'users'} element={<UsersComponent/>}/>
+                <Route path={'posts'} element={<PostsComponent/>}/>
+                <Route path={'comments'} element={<PostsComponent/>}/>
+                <Route path={'products'} element={<ProductsComponent/>}/>
+
+            </Route>
+        </Routes>
+    </BrowserRouter>)
