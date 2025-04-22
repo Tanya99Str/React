0dc937fbb6310977386a452b69433511ffcb15ee
Tetjana Strutynska <tetjana00iris@gmail.com>
2025-04-22 00:51:24 -0300

    #knZgVIwvqd

--- src/components/users-dummyjson-component/UsersDummyjsonComponent.tsx
+++ src/components/users-dummyjson-component/UsersDummyjsonComponent.tsx
@@ -1,29 +1,34 @@
 import './UsersDummyjsonComponent.css';
 import {useEffect, useState} from "react";
 import {UserDummyjsonModel} from "../../models/UserDummyjsonModel.ts";
-import {loadUserDummyjson} from "../../services/api.service.ts";
+import {loadUsersWithPagination} from "../../services/api.service.ts";
 import {UserDummyjsonComponent} from "../user-dummyjson-component/UserDummyjsonComponent.tsx";
-import {Outlet} from "react-router-dom";
+import {PaginationComponent} from "../pagination-component/PaginationComponent.tsx";
+import {useSearchParams} from "react-router-dom";
+// import {Outlet} from "react-router-dom";
 
 export const UsersDummyjsonComponent = () => {
 
     const [users, setUsers] = useState<UserDummyjsonModel[]>([]);
+    const [searchParams] = useSearchParams({page: '1'});
     useEffect(() => {
-        loadUserDummyjson().then(val => setUsers(val));
-    }, []);
+        const currentPage = searchParams.get('page') || '1';
+        loadUsersWithPagination(currentPage).then(val => setUsers(val));
+    }, [searchParams]);
 
     return (
-        <>
-            <Outlet/>
+        <div style={{padding: '20px'}}>
+            {/*<Outlet/>*/}
             <div className={'users-list'}>
                 {
                     users.map(user =>
-                        <div className="one-elem">
-                            <UserDummyjsonComponent user={user} key={user.id}/>
+                        <div className="one-elem" key={user.id}>
+                            <UserDummyjsonComponent user={user}/>
                         </div>
                     )
                 }
             </div>
-        </>
+            <PaginationComponent/>
+        </div>
     );
 };
